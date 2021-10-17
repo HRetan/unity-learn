dba4cef932e1ece1454d8d22f49a8ba31813b354
HRetan <kairencer@gmail.com>
2021-10-17 18:23:37 +0900

    몬스터 이동 로직 구현 : 포인트 위치까지 이동

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -6,7 +6,8 @@ public class Enemy : MonoBehaviour
 {
     private GameObject goPlayer;
     private Player cPlayer;
-    private Vector2 vecPosition = new Vector2(10, 10);
+    private Vector2 vecPoint = new Vector2(10, 10);
+    private float pointDistance = 0.1f;
     private Animator anim;
 
     // stage 관리하는 오브젝트에 접근해서 스테이지 별 강화 적용
@@ -25,10 +26,20 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(vecPosition != transform.position) {
-            transform.position += new Vector3(0, -1.0f, 0);
-            
+        float vecDistance = vecPoint.position - transform.position;
+        if(eState == UNIT_STATE.WALK) {
+            if(vecDistance.sqrMagnitude >=  pointDistance * pointDistance) {
+                transform.position += new Vector3(0, -1.0f, 0);
+            } else {
+                transform.position = vecPoint.position;
+                Idle();
+            }
         }
+    }
+
+    void Idle() {
+    eState = UNIT_STATE.IDLE;
+    anim.setBool("isIdle", true);        
     }
 
     public override void Attack() {
